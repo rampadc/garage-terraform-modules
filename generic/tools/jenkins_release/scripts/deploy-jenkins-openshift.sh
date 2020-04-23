4af0cc6663530b2ad18925b9bb1140f3dba6974d
Sean Sundberg <seansund@us.ibm.com>
2020-04-23 09:38:42 -0500

    jenkins module: Updates deploy logic for OpenShift

--- generic/tools/jenkins_release/scripts/deploy-jenkins-openshift.sh
+++ generic/tools/jenkins_release/scripts/deploy-jenkins-openshift.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 CHART_DIR=$(cd "${SCRIPT_DIR}/../charts"; pwd -P)
 
-CI_NAMESPACE="$1"
-TOOLS_NAMESPACE="$2"
+TOOLS_NAMESPACE="$1"
+SERVER_URL="$2"
 
 if [[ -z "${TMP_DIR}" ]]; then
   TMP_DIR=./tmp
@@ -13,20 +13,7 @@ fi
 mkdir -p ${TMP_DIR}
 
 YAML_OUTPUT=${TMP_DIR}/jenkins-config.yaml
-
-echo "Creating jenkins-ephemeral instance"
-oc new-app jenkins-ephemeral -n "${CI_NAMESPACE}"
-
-sleep 5
-
-JENKINS_HOST=$(oc get route jenkins -n "${CI_NAMESPACE}" -o jsonpath='{ .spec.host }')
-JENKINS_URL="https://${JENKINS_HOST}"
-
-if [[ -n "${SERVER_URL}" ]]; then
-  PIPELINE_URL="${SERVER_URL}/console/projects"
-else
-  PIPELINE_URL="${JENKINS_URL}"
-fi
+PIPELINE_URL="${SERVER_URL}/console/projects"
 
 helm template "${CHART_DIR}/pipeline-config" \
     --name "pipeline-config" \
@@ -34,6 +21,3 @@ helm template "${CHART_DIR}/pipeline-config" \
     --set "pipeline.url=${PIPELINE_URL}" \
     --set "pipeline.tls=true" > ${YAML_OUTPUT}
 kubectl apply --namespace "${TOOLS_NAMESPACE}" -f ${YAML_OUTPUT}
-
-echo "*** Waiting for Jenkins on ${JENKINS_URL}"
-"${SCRIPT_DIR}/waitForEndpoint.sh" "${JENKINS_URL}/login" 150 12
